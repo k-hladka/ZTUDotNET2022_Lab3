b3627dbdd4f5da900641a025b77aba0ab04bc7af
Kateryna Hladka <zipz211_gkv@student.ztu.edu.ua>
2023-02-01 14:59:12 +0200

    A program that has extension methods, has been created

---
+++ String/ClassesExtensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String.ClassesExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Invert(this string str)
+        {
+            string resultString = "";
+            for(int i= str.Length-1; i>=0; i--)
+                resultString += str[i];
+            return resultString;
+        }
+        public static int CountChar(this string str, char c)
+        {
+            int count = 0;
+            foreach(char i in str)
+                if (i == c)
+                    count++;
+            return count;
+        }
+    }
+}
---
+++ String/Program.cs
@@ -0,0 +1,18 @@
+﻿using String.ClassesExtensions;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Інвертування рядка: ");
+Console.ResetColor();
+
+string str = "Британські пушистики схожі на плюшевих ведмежат. Це компактні і при цьому потужні коти.";
+Console.WriteLine(str.Invert());
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Підрахунок кількості входжень символа 'і' в рядок: ");
+Console.ResetColor();
+
+Console.WriteLine(str.CountChar('і'));
